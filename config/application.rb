5fce70aad18ea364150bfc0fb87ced3d7c7590c1
Saroar <saroar9@gmail.com>
2016-05-03 13:03:23 +0300

    clean gemfile and add slim template as default

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module Zips
   class Application < Rails::Application
+    # Make default slim template_engine
+    config.generators do |g|
+      g.template_engine :slim
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
