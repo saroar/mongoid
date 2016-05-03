0b2b30b78ad55714e7d9e0ad6c97582b89234b50
Saroar <saroar9@gmail.com>
2016-05-03 13:39:10 +0300

    update zip index for paginate and create new method get sort hash

--- app/controllers/zips_controller.rb
+++ app/controllers/zips_controller.rb
@@ -4,7 +4,12 @@ class ZipsController < ApplicationController
   # GET /zips
   # GET /zips.json
   def index
-    @zips = Zip.all
+    #@zips = Zip.all
+    #@zips = Zip.paginate(params)
+
+    args=params.clone                      #update a clone of params
+    args[:sort]=get_sort_hash(args[:sort]) #replace sort with hash
+    @zips = Zip.paginate(args)
   end
 
   # GET /zips/1
@@ -70,5 +75,20 @@ class ZipsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def zip_params
       params.require(:zip).permit(:id, :city, :state, :population)
+    end
+
+    #create a hash sort spec from query param
+    #sort=state:1,city,population:-1
+    #{state:1, city:1, population:-1}
+    def get_sort_hash(sort)
+      order={}
+      if (!sort.nil?)
+        sort.split(",").each do |term|
+          args=term.split(":")
+          dir = args.length<2 || args[1].to_i >= 0 ? 1 : -1
+          order[args[0]] = dir
+        end
+      end
+      return order
     end
 end
